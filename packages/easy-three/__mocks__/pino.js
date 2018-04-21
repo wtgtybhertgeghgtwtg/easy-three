8745be9920306ba18620f0b56a7bb92c2f5c2548
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-04-21 13:08:39 -0700

    style: log fatal errors as fatal

--- packages/easy-three/__mocks__/pino.js
+++ packages/easy-three/__mocks__/pino.js
@@ -1,14 +1,19 @@
 let errorFunc = jest.fn();
+let fatalFunc = jest.fn();
 let infoFunc = jest.fn();
 let warnFunc = jest.fn();
 
 const pino = jest.fn(() => ({
   error: errorFunc,
+  fatal: fatalFunc,
   info: infoFunc,
   warn: warnFunc,
 }));
 pino.__setErrorFunc = newErrorFunc => {
   errorFunc = newErrorFunc;
+};
+pino.__setFatalFunc = newFatalFunc => {
+  fatalFunc = newFatalFunc;
 };
 pino.__setInfoFunc = newInfoFunc => {
   infoFunc = newInfoFunc;
