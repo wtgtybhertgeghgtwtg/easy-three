2f35d29c9c17455fcef12dde34a737348a988444
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-05-04 20:12:04 -0700

    refactor: bump `env-and-files` (#34)
    BREAKING CHANGE: `init` now returns a Promise that resolves once the
    application is initialized.

--- packages/easy-three/__mocks__/death.js
+++ packages/easy-three/__mocks__/death.js
@@ -1,13 +1,8 @@
 let dyingFunc;
-let onDeath = jest.fn();
 
 const death = jest.fn(newDyingFunc => {
   dyingFunc = newDyingFunc;
-  onDeath();
 });
 death.__die = (signal, error) => dyingFunc(signal, error);
-death.__setOnDeath = newOnDeath => {
-  onDeath = newOnDeath;
-};
 
 module.exports = death;
--- packages/easy-three/__tests__/init.test.js
+++ packages/easy-three/__tests__/init.test.js
@@ -41,39 +41,30 @@ describe('init', () => {
     });
 
     // Terrible test name.
-    it('defaults to non-pretty "info".', done => {
-      init({}, () => {
-        expect(pino).toHaveBeenCalledWith({
-          level: 'info',
-          prettyPrint: false,
-        });
-        done();
-        return () => {};
+    it('defaults to non-pretty "info".', async () => {
+      await init({}, () => () => {});
+      expect(pino).toHaveBeenCalledWith({
+        level: 'info',
+        prettyPrint: false,
       });
     });
 
-    it('passes "LOG_LEVEL" as the log level.', done => {
+    it('passes "LOG_LEVEL" as the log level.', async () => {
       const logLevel = 'trace';
       process.env.LOG_LEVEL = logLevel;
-      init({}, () => {
-        expect(pino).toHaveBeenCalledWith({
-          level: logLevel,
-          prettyPrint: false,
-        });
-        done();
-        return () => {};
+      await init({}, () => () => {});
+      expect(pino).toHaveBeenCalledWith({
+        level: logLevel,
+        prettyPrint: false,
       });
     });
 
-    it('is pretty if "LOG_PRETTY" equals true.', done => {
+    it('is pretty if "LOG_PRETTY" equals true.', async () => {
       process.env.LOG_PRETTY = 'true';
-      init({}, () => {
-        expect(pino).toHaveBeenCalledWith({
-          level: 'info',
-          prettyPrint: true,
-        });
-        done();
-        return () => {};
+      await init({}, () => () => {});
+      expect(pino).toHaveBeenCalledWith({
+        level: 'info',
+        prettyPrint: true,
       });
     });
   });
@@ -85,15 +76,17 @@ describe('init', () => {
 
     const variableName = 'SOME_CONFIG_VARIABLE';
 
-    it('logs a ConfigError if it there is one.', done => {
+    it('logs a ConfigError if it there is one and rejects.', async () => {
       const start = jest.fn();
-      pino.__setFatalFunc(error => {
-        expect(error.message).toEqual('Configuration could not be loaded.');
-        expect(start).not.toHaveBeenCalled();
-        done();
-      });
       delete process.env[variableName];
-      init({groupOne: {propOne: {required: true, variableName}}}, start);
+      await expect(
+        init({groupOne: {propOne: {required: true, variableName}}}, start),
+      ).rejects.toThrow('Configuration could not be loaded.');
+      expect(pino.__logger.fatal).toHaveBeenCalledWith(
+        expect.any(Error),
+        'Configuration could not be loaded.  Application will not start.',
+      );
+      expect(start).not.toHaveBeenCalled();
     });
 
     it('passes loaded config to "start".', done => {
@@ -106,51 +99,48 @@ describe('init', () => {
       });
     });
 
-    it('passes the logger to "start".', done => {
+    it('passes the logger to "start".', async () => {
       const message = 'Some message.';
-      pino.__setInfoFunc(info => {
-        if (info === message) {
-          done();
-        }
-      });
-      init({}, (config, logger) => {
+      await init({}, (config, logger) => {
         logger.info(message);
         return () => {};
       });
+      expect(pino.__logger.info).toHaveBeenCalledWith(message);
     });
 
-    it('logs the error if "start" throws.', done => {
+    it('logs the error and rejects if "start" throws.', async () => {
       const message = 'Some message.';
-      pino.__setFatalFunc(error => {
-        expect(error.message).toEqual(message);
-        done();
-      });
-      init({}, () => {
-        throw new Error(message);
-      });
+      await expect(
+        init({}, () => {
+          throw new Error(message);
+        }),
+      ).rejects.toThrow(message);
+      expect(pino.__logger.fatal).toHaveBeenCalledWith(
+        expect.any(Error),
+        'An error was encountered while attempting to initialize.',
+      );
     });
 
-    it('logs the error if "start" rejects.', done => {
+    it('logs the error and rejects if "start" rejects.', async () => {
       const message = 'Some message.';
-      pino.__setFatalFunc(error => {
-        expect(error.message).toEqual(message);
-        done();
-      });
-      init({}, async () => {
-        throw new Error(message);
-      });
+      await expect(
+        init({}, async () => {
+          throw new Error(message);
+        }),
+      ).rejects.toThrow(message);
+      expect(pino.__logger.fatal).toHaveBeenCalledWith(
+        expect.any(Error),
+        'An error was encountered while attempting to initialize.',
+      );
     });
 
-    it('gives a warning if "start" does not return a function or a Promise that resolves to a function.', done => {
-      pino.__setWarnFunc(warn => {
-        expect(warn).toEqual(
-          '"start" should return a function or a Promise resolving to a function.',
-        );
-        expect(death).not.toHaveBeenCalled();
-        done();
-      });
+    it('gives a warning if "start" does not return a function or a Promise that resolves to a function.', async () => {
       // $FlowFixMe
-      init({}, () => {});
+      await init({}, () => {});
+      expect(pino.__logger.warn).toHaveBeenCalledWith(
+        '"start" should return a function or a Promise resolving to a function.',
+      );
+      expect(death).not.toHaveBeenCalled();
     });
   });
 
@@ -159,50 +149,48 @@ describe('init', () => {
       jest.clearAllMocks();
     });
 
-    it('calls "shutdown" on death.', done => {
+    it('calls "shutdown" on death.', async () => {
       const shutdown = jest.fn();
-      death.__setOnDeath(() => {
-        death.__die();
-        expect(shutdown).toHaveBeenCalled();
-        done();
-      });
-      init({}, () => shutdown);
+      await init({}, () => shutdown);
+      expect(shutdown).not.toHaveBeenCalled();
+      await death.__die();
+      expect(shutdown).toHaveBeenCalled();
     });
 
-    it('logs the error if "shutdown" passes one.', done => {
-      const message = 'Some message.';
-      pino.__setFatalFunc(error => {
-        expect(error.message).toEqual(message);
-        done();
-      });
-      death.__setOnDeath(() => death.__die(undefined, new Error(message)));
-      init({}, () => () => {});
+    it('logs the error if "shutdown" passes one.', async () => {
+      const error = new Error('Some message.');
+      await init({}, () => () => {});
+      await death.__die(undefined, error);
+      expect(pino.__logger.fatal).toHaveBeenCalledWith(
+        error,
+        'Aborting due to fatal error.',
+      );
     });
 
-    it('logs the error if "shutdown" throws.', done => {
-      const message = 'Some message.';
+    it('logs the error if "shutdown" throws.', async () => {
+      const error = new Error('Some message.');
       const shutdown = () => {
-        throw new Error(message);
+        throw error;
       };
-      pino.__setErrorFunc(error => {
-        expect(error.message).toEqual(message);
-        done();
-      });
-      death.__setOnDeath(death.__die);
-      init({}, () => shutdown);
+      await init({}, () => shutdown);
+      await death.__die();
+      expect(pino.__logger.error).toHaveBeenCalledWith(
+        error,
+        'An error was encountered while attempting to shut down.',
+      );
     });
 
-    it('logs the error if "shutdown" rejects.', done => {
-      const message = 'Some message.';
+    it('logs the error if "shutdown" rejects.', async () => {
+      const error = new Error('Some message.');
       const shutdown = async () => {
-        throw new Error(message);
+        throw error;
       };
-      pino.__setErrorFunc(error => {
-        expect(error.message).toEqual(message);
-        done();
-      });
-      death.__setOnDeath(death.__die);
-      init({}, () => shutdown);
+      await init({}, () => shutdown);
+      await death.__die();
+      expect(pino.__logger.error).toHaveBeenCalledWith(
+        error,
+        'An error was encountered while attempting to shut down.',
+      );
     });
   });
 });
